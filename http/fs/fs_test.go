535de5f766e92161d89e5e4a3108329cfd052e3d
xushiwei <x@goplus.org>
2023-09-13 19:11:55 +0800

    TestUnionFS

--- http/fs/fs_test.go
+++ http/fs/fs_test.go
@@ -1,9 +1,25 @@
 package fs
 
 import (
+	"io"
 	"os"
 	"testing"
 )
+
+func TestUnionFS(t *testing.T) {
+	fs := Union(Root(), Files("fs/file.go", "/not-exist/noop.nil"), FilesWithContent("fs/file.go", "a"))
+	f, err := fs.Open("/fs/file.go")
+	if err != nil {
+		t.Fatal("Union.Open:", err)
+	}
+	if b, err := io.ReadAll(f); err != nil || string(b) != "a" {
+		t.Fatal("UnionFS ReadAll:", string(b), err)
+	}
+	_, err = fs.Open("/not-exist/foo")
+	if !os.IsNotExist(err) {
+		t.Fatal("fs.Open:", err)
+	}
+}
 
 func TestParentFS(t *testing.T) {
 	fs := FilesWithContent("a.txt", "a")
