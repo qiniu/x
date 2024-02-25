bba26cc01e7de9fc9f3a83142a734b6303ae289c
xushiwei <x@goplus.org>
2024-02-25 21:18:19 +0800

    bufiox

--- bufiox/seek.go
+++ bufiox/seek.go
@@ -1,3 +1,19 @@
+/*
+ Copyright 2020 Qiniu Limited (qiniu.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
 package bufiox
 
 import (
@@ -24,7 +40,6 @@ func NewReader(rd io.ReadSeeker) *Reader {
 // NewReaderSize returns a new Reader whose buffer has at least the specified
 // size. If the argument io.Reader is already a Reader with large enough size,
 // it returns the underlying Reader.
-//
 func NewReaderSize(rd io.ReadSeeker, size int) *Reader {
 	b, ok := rd.(*Reader)
 	if ok && b.Size() >= size {
@@ -39,7 +54,6 @@ func NewReaderSize(rd io.ReadSeeker, size int) *Reader {
 // SeekCurrent means relative to the current offset, and SeekEnd means
 // relative to the end. Seek returns the new offset relative to the start
 // of the file and an error, if any.
-//
 func (r *Reader) Seek(offset int64, whence int) (int64, error) {
 	return Seek(&r.Reader, offset, whence)
 }
